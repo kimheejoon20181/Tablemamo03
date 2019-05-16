a0a352e36ea3abbfd5be9db34fef39ce1c17c672
D7703_22 <d7703_22@promise.local>
2019-05-16 12:53:31 +0900

    commit 5 / 16

--- Tablemamo03/ViewController.swift
+++ Tablemamo03/ViewController.swift
@@ -8,11 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var data = ["A","B","C","D","E"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let row = indexPath.row
+        
+        if row % 2 == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "first", for: indexPath)
+            cell.textLabel?.text = data[row]
+            cell.detailTextLabel?.text = "row = \(row)"
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "second", for: indexPath)
+            let row = indexPath.row
+            
+            cell.textLabel?.text = data[row]
+            cell.detailTextLabel?.text = "row = \(row)"
+            return cell
+        }
+        
     }
 
 
